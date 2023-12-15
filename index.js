ef69cb75fd369f5b34438a4922a481d428de0fec
MsWakesho <wakeshosonja@gmail.com>
2023-12-15 18:27:11 +0300

    Completed Assignments

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate"){ //roller-skate here is the default argument
+    return `This Saturday, I want to ${activity}!`; 
+}
+console.log(saturdayFun("bathe my dog !")) //This is an example of overiding an default argument
+
+function mondayWork(activity = "go to the office"){
+
+    return `This Monday, I will ${activity}.`;
+
+}
+console.log(mondayWork("work from home"))
+
+
+function wrapAdjective(symbol) {
+    return function(adjective) {
+      return `You are ${symbol}${adjective}${symbol}!`;
+    }
+  }
